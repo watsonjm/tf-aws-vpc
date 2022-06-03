9ab13ed79ab9a0d210dd3865884939006e483f88
John Watson <62905388+watsonjm@users.noreply.github.com>
2022-06-03 11:48:19 -0400

    Added AZ data lookup

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,12 @@
 data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 data "aws_partition" "current" {}
-
+data "aws_availability_zones" "all" {
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
 resource "aws_vpc" "mainvpc" {
   cidr_block           = var.cidr_block
   enable_dns_hostnames = var.enable_dns_hostnames
